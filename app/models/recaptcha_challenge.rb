79907f4972ce8b537e56c3013db519291a6bd774
Olivier <ducrouxolivier@gmail.com>
2025-04-26 20:35:22 +0200

    fix

--- app/models/recaptcha_challenge.rb
+++ app/models/recaptcha_challenge.rb
@@ -2,7 +2,7 @@ class RecaptchaChallenge < ApplicationRecord
   after_save :create_python_file
 
   def create_python_file
-    File.open(Rails.root.join("tmp", "python_scripts", "recaptcha_challenge_#{id}.py"), "w") do |file|
+    File.open(Rails.root.join("python_scripts", "recaptcha_challenge_#{id}.py"), "w") do |file|
       file.write(python_script.gsub(/^ {6}/, ""))
     end
   end
